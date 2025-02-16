053adad76420f704093d5ba8dde98b8ef3ea3d3f
Owen <owen.downs@wsu.edu>
2025-02-15 17:39:14 -0800

    Added a basic GameScene with a cursor-following salt shaker.

---
+++ Assets/Scripts/SaltShaker.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SaltShaker : MonoBehaviour
+{
+    // Members
+
+    public float speed = 10f;
+    public float deadzone = 1;
+    public float smoothness = 100;
+    private Rigidbody2D rigidBody;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameObject.transform.position = new Vector2(0, 0);
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 distance = mousePosition - rigidBody.position;
+        if (distance.magnitude > deadzone)
+        {
+            Vector2 direction = (mousePosition - rigidBody.position).normalized;
+            rigidBody.linearVelocity = direction * speed * (distance.magnitude / smoothness);
+        }
+        else
+        {
+            rigidBody.linearVelocity = Vector2.zero;
+        }
+    }
+}
