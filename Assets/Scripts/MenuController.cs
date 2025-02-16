26e89fd84b55b5dd5a014fcbb5748a27f60ebe1a
Owen <riverdowns80@gmail.com>
2025-02-16 03:22:54 -0800

    Fixed merge conflictions

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Load game scene
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    // Quit the game
+    public void QuitGame()
+    {
+    #if UNITY_EDITOR
+            // This works in the Unity Editor
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        // This works in the built application
+        Application.Quit();
+    #endif
+    }
+}
+
