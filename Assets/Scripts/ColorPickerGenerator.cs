057547bd30a2c0fa4fb0904274b7b12c4c5b2a0f
Owen <riverdowns80@gmail.com>
2025-02-16 05:36:35 -0800

    Fixed color picker handling

--- Assets/Scripts/ColorPickerGenerator.cs
+++ Assets/Scripts/ColorPickerGenerator.cs
@@ -6,6 +6,8 @@ public class ColorPickerGenerator : MonoBehaviour
     public GameObject colorPicker;
     private GameObject[] currentColorPickers = new GameObject[5];
     private Color[] colorOptions = new Color[5];
+    private int limit = 5;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,31 +16,26 @@ public class ColorPickerGenerator : MonoBehaviour
         colorOptions[2] = Color.blue;
         colorOptions[3] = Color.green;
         colorOptions[4] = Color.gray;
-        GenerateColors();
+        for(int i = 0; i < 5; i++)
+        {
+            GameObject newColorPicker = Instantiate(colorPicker, new Vector3(-9.5f + (i * 1.5f), 4.2f, 4), Quaternion.identity);
+            newColorPicker.GetComponent<SpriteRenderer>().color = colorOptions[i];
+            currentColorPickers[i] = newColorPicker;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void GenerateColors()
-    {
-        ResetColorPickers();
-
-        for (int i = 0; i < 5; i++)
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            GameObject newColorPicker = Instantiate(colorPicker, new Vector3(-9.5f + (i * 1.5f), 4.2f, 4), Quaternion.identity);
-            newColorPicker.GetComponent<SpriteRenderer>().color = colorOptions[i];
-            currentColorPickers.Append(newColorPicker);
+            ResetColorPickers();
         }
     }
 
     public void GenerateColors(Color[] newColors)
     {
         ResetColorPickers();
-        int limit;
         if (newColors.Length < 5)
         {
             limit = newColors.Length;
@@ -50,21 +47,17 @@ public class ColorPickerGenerator : MonoBehaviour
 
         for (int i = 0; i < limit; i++)
         {
-            GameObject newColorPicker = Instantiate(colorPicker, new Vector3(-9.5f + (i * 1.5f), 4.2f, 4), Quaternion.identity);
-            newColorPicker.GetComponent<SpriteRenderer>().color = newColors[i];
-            currentColorPickers.Append(newColorPicker);
+            currentColorPickers[i].GetComponent<SpriteRenderer>().color = newColors[i];
+            currentColorPickers[i].SetActive(true);
         }
     }
 
     private void ResetColorPickers()
     {
-        for (int i = 0; i < currentColorPickers.Length; i++)
+        print("Resetting Color Pickers");
+        for (int i = 0; i < limit; i++)
         {
-            if (currentColorPickers[i] != null)
-            {
-                Destroy(currentColorPickers[i]);
-            }
+            currentColorPickers[i].SetActive(false);
         }
-        currentColorPickers = new GameObject[5];
     }
 }
