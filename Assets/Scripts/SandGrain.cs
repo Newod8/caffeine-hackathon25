50d70cd213dff43ddb4aad72b4b90030ff444ea8
Owen <owen.downs@wsu.edu>
2025-02-15 20:16:23 -0800

    Added reset feature and added killbox and sand shaker to prefab assets.

--- Assets/Scripts/SandGrain.cs
+++ Assets/Scripts/SandGrain.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using UnityEngine;
 
 public class SandGrain : MonoBehaviour
@@ -21,6 +22,10 @@ public class SandGrain : MonoBehaviour
                 this.Freeze();
                 this.isFrozen = true;
             }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Destroy(gameObject);
         }
     }
 
